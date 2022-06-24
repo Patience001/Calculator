4d266fb4c56a7effe4724677acbc6180b66cf28f
Patience001 <patienceinyang1111@gmail.com>
2022-06-24 16:04:44 +0100

    Calculator

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');  
+  
+const num1 = parseFloat(prompt ('Enter the first number: '));  
+const num2 = parseFloat(prompt ('Enter the second number: '));  
+  
+let result;
+  
+if (operator == '+') { 
+    result = num1 + num2;  
+}  
+else if (operator == '-') {  
+    result = num1 - num2;  
+}  
+else if (operator == '*') {  
+    result = num1 * num2;  
+}  
+else {  
+    result = num1 / num2;   
+}   
+window.alert(" Answer is" + " " + result);  
